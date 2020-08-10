5f8aa88096be70d8db3a895c64d979b34e6162a2
shadezs <davila_salazar_carlo@student.smc.edu>
2020-08-10 13:52:42 -0700

    Fix API key function

--- routes/zipcode.js
+++ routes/zipcode.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const axios = require('axios');
 const router = express.Router();
+const { YELP_API_KEY } = process.env;
 
 /*  GET zipcode page  */
 
 router.get('/', (req, res, next) => {
-  res.render('businesses/zipcodepage.ejs', { title: 'Zip Code Page' });
+    res.render('businesses/zipcodepage.ejs', { title: 'Zip Code Page' });
 });
 
 /* GET yelpJSON.businesses */
@@ -40,34 +41,34 @@ router.get('/', (req, res, next) => {
 });*/
 
 /* GET yelpJSON.businesses */
-router.post('/', async (req, res, next) => {
-  const userzip = req.body.userzip;
-  console.log("Post request received");
-  console.log(req.body.userzip);
+router.post('/', async(req, res, next) => {
+    const userzip = req.body.userzip;
+    console.log("Post request received");
+    console.log(req.body.userzip);
 
-  var config = {
-    method: 'get',
-    url: 'https://api.yelp.com/v3/businesses/search?term=restaurants&location=' + userzip + '&price=1&sort_by=rating&limit=10',
-    headers: {
-      'Authorization': 'Bearer dfeBZkngziUaBNGBh9N05JV7npLPtIFq2vhmqVRiUrEO3XAVnZHyjxvxCQraXCbQCP5lmeaU3Yho9ae_NV5Vsh3f539FFXTnVctpYrTJuojBwU25A9V-btTg17spX3Yx'
+    var config = {
+        method: 'get',
+        url: 'https://api.yelp.com/v3/businesses/search?term=restaurants&location=' + userzip + '&price=1&sort_by=rating&limit=10',
+        headers: {
+            'Authorization': YELP_API_KEY
+        }
+    };
+    console.log(config.url);
+
+    try {
+
+        const { data: yelpJSON } = await axios(config);
+
+        const businesses = yelpJSON.businesses;
+        //console.log(businessesArray);
+        //console.log(yelpJSON);
+        res.render('businesses/restaurants.ejs', {
+            businesses,
+        });
+
+    } catch (e) {
+        console.log(e);
     }
-  };
-  console.log(config.url);
-
-  try {
-
-    const { data: yelpJSON } = await axios(config);
-
-    const businesses = yelpJSON.businesses;
-    //console.log(businessesArray);
-    //console.log(yelpJSON);
-    res.render('businesses/restaurants.ejs', {
-      businesses,
-    });
-
-  } catch (e) {
-    console.log(e);
-  }
 
 });
 
