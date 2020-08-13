fe5363c61cd098310b7d1069f1dd34b5148c0bd7
Carlos <davila_salazar_carlo@student.smc.edu>
2020-08-12 18:26:53 -0700

    Update mapboxconfig.js

--- config/mapboxconfig.js
+++ config/mapboxconfig.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-mapboxgl.accessToken = 'pk.eyJ1Ijoic2hhZGVzeiIsImEiOiJja2NiNXd0bHEwMTNyMnJtenRybnoxZjRyIn0.4lceMjYIVlaaIT9E8FoSyw'
+mapboxgl.accessToken = 
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11'
