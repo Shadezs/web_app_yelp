eae23da44099ab31437aae486496ba4c73a4748f
shadezs <davila_salazar_carlo@student.smc.edu>
2020-08-12 17:12:06 -0700

    Fix API key visibility

--- config/firebase.js
+++ config/firebase.js
@@ -3,14 +3,14 @@ require('firebase/auth')
 require('firebase/firestore')
 
 const firebaseConfig = {
-    apiKey: "AIzaSyByFY74ojNshwexBW4vjYJXpexrfedpLx4",
-    authDomain: "yelp-web-app-45122.firebaseapp.com",
-    databaseURL: "https://yelp-web-app-45122.firebaseio.com",
-    projectId: "yelp-web-app-45122",
-    storageBucket: "yelp-web-app-45122.appspot.com",
-    messagingSenderId: "1092438213910",
-    appId: "1:1092438213910:web:1ebdd000d5cff7ec2ffc37",
-    measurementId: "G-LB77RGXHXQ"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
 };
 // Initialize Firebase
 class Firebase {
