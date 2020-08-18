800e809c6a4f2afd1ecb65dc16d3b6e9f0ed2bc5
shadezs <davila_salazar_carlo@student.smc.edu>
2020-08-17 17:50:11 -0700

    Added map pop up with name of resturant

--- public/js/mapboxconfig.js
+++ public/js/mapboxconfig.js
@@ -4,7 +4,7 @@ mapboxgl.accessToken = //// use API KEY HERE
         style: 'mapbox://styles/shadesz/ckdxii11i16sy19phd8a7sdp6'
 
     });
-console.log(busines)
+console.log(array_bussines[0])
 map.on('load', function() {
     map.loadImage(
         'https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',
@@ -20,83 +20,83 @@ map.on('load', function() {
                     'features': [{
                         'type': 'Feature',
                         'properties': {
-                            'description': '<strong>Make it Mount Pleasant</strong><p>Make it Mount Pleasant is a handmade and vintage market and afternoon of live entertainment and kids activities. 12:00-6:00 p.m.</p>'
+                            'description': `<strong>${array_bussines[0].name}</strong><p></p>`
                         },
                         'geometry': {
                             'type': 'Point',
-                            'coordinates': [busines.latitude, busines.longitude]
+                            'coordinates': [array_bussines[0].longitude, array_bussines[0].latitude]
                         }
                     }, {
                         'type': 'Feature',
                         'properties': {
-                            'description': '<strong>Mad Men Season Five Finale Watch Party</strong><p>Head to Lounge 201 (201 Massachusetts Avenue NE) Sunday for a Mad Men Season Five Finale Watch Party, complete with 60s costume contest, Mad Men trivia, and retro food and drink. 8:00-11:00 p.m. $10 general admission, $20 admission and two hour open bar.</p>'
+                            'description': `<strong>${array_bussines[1].name}</strong><p>Head to Lounge 201 (201 Massachusetts Avenue NE) Sunday for a Mad Men Season Five Finale Watch Party, complete with 60s costume contest, Mad Men trivia, and retro food and drink. 8:00-11:00 p.m. $10 general admission, $20 admission and two hour open bar.</p>`
                         },
                         'geometry': {
                             'type': 'Point',
-                            'coordinates': [-77.003168, 38.894651]
+                            'coordinates': [array_bussines[1].longitude, array_bussines[1].latitude]
                         }
                     }, {
                         'type': 'Feature',
                         'properties': {
-                            'description': '<strong>Big Backyard Beach Bash and Wine Fest</strong><p>EatBar (2761 Washington Boulevard Arlington VA) is throwing a Big Backyard Beach Bash and Wine Fest on Saturday, serving up conch fritters, fish tacos and crab sliders, and Red Apron hot dogs. 12:00-3:00 p.m. $25.</p>'
+                            'description': `<strong>${array_bussines[2].name}</strong><p>EatBar (2761 Washington Boulevard Arlington VA) is throwing a Big Backyard Beach Bash and Wine Fest on Saturday, serving up conch fritters, fish tacos and crab sliders, and Red Apron hot dogs. 12:00-3:00 p.m. $25.</p>`
                         },
                         'geometry': {
                             'type': 'Point',
-                            'coordinates': [-77.090372, 38.881189]
+                            'coordinates': [array_bussines[2].longitude, array_bussines[2].latitude]
                         }
                     }, {
                         'type': 'Feature',
                         'properties': {
-                            'description': '<strong>Ballston Arts & Crafts Market</strong><p>The Ballston Arts & Crafts Market sets up shop next to the Ballston metro this Saturday for the first of five dates this summer. Nearly 35 artists and crafters will be on hand selling their wares. 10:00-4:00 p.m.</p>'
+                            'description': `<strong>${array_bussines[3].name}</strong><p>The Ballston Arts & Crafts Market sets up shop next to the Ballston metro this Saturday for the first of five dates this summer. Nearly 35 artists and crafters will be on hand selling their wares. 10:00-4:00 p.m.</p>`
                         },
                         'geometry': {
                             'type': 'Point',
-                            'coordinates': [-77.111561, 38.882342]
+                            'coordinates': [array_bussines[3].longitude, array_bussines[3].latitude]
                         }
                     }, {
                         'type': 'Feature',
                         'properties': {
-                            'description': "<strong>Seersucker Bike Ride and Social</strong><p>Feeling dandy? Get fancy, grab your bike, and take part in this year's Seersucker Social bike ride from Dandies and Quaintrelles. After the ride enjoy a lawn party at Hillwood with jazz, cocktails, paper hat-making, and more. 11:00-7:00 p.m.</p>"
+                            'description': `<strong>${array_bussines[4].name}</strong><p>Feeling dandy? Get fancy, grab your bike, and take part in this year's Seersucker Social bike ride from Dandies and Quaintrelles. After the ride enjoy a lawn party at Hillwood with jazz, cocktails, paper hat-making, and more. 11:00-7:00 p.m.</p>`
                         },
                         'geometry': {
                             'type': 'Point',
-                            'coordinates': [-77.052477, 38.943951]
+                            'coordinates': [array_bussines[4].longitude, array_bussines[4].latitude]
                         }
                     }, {
                         'type': 'Feature',
                         'properties': {
-                            'description': '<strong>Capital Pride Parade</strong><p>The annual Capital Pride Parade makes its way through Dupont this Saturday. 4:30 p.m. Free.</p>'
+                            'description': `<strong>${array_bussines[5].name}</strong><p>The annual Capital Pride Parade makes its way through Dupont this Saturday. 4:30 p.m. Free.</p>`
                         },
                         'geometry': {
                             'type': 'Point',
-                            'coordinates': [-77.043444, 38.909664]
+                            'coordinates': [array_bussines[5].longitude, array_bussines[5].latitude]
                         }
                     }, {
                         'type': 'Feature',
                         'properties': {
-                            'description': '<strong>Muhsinah</strong><p>Jazz-influenced hip hop artist Muhsinah plays the Black Cat (1811 14th Street NW) tonight with Exit Clov and Gods’illa. 9:00 p.m. $12.</p>'
+                            'description': `<strong>${array_bussines[6].name}</strong><p>Jazz-influenced hip hop artist Muhsinah plays the Black Cat (1811 14th Street NW) tonight with Exit Clov and Gods’illa. 9:00 p.m. $12.</p>`
                         },
                         'geometry': {
                             'type': 'Point',
-                            'coordinates': [-77.031706, 38.914581]
+                            'coordinates': [array_bussines[6].longitude, array_bussines[6].latitude]
                         }
                     }, {
                         'type': 'Feature',
                         'properties': {
-                            'description': "<strong>A Little Night Music</strong><p>The Arlington Players' production of Stephen Sondheim's <em>A Little Night Music</em> comes to the Kogod Cradle at The Mead Center for American Theater (1101 6th Street SW) this weekend and next. 8:00 p.m.</p>"
+                            'description': `<strong>${array_bussines[7].name}</strong><p>The Arlington Players' production of Stephen Sondheim's <em>A Little Night Music</em> comes to the Kogod Cradle at The Mead Center for American Theater (1101 6th Street SW) this weekend and next. 8:00 p.m.</p>`
                         },
                         'geometry': {
                             'type': 'Point',
-                            'coordinates': [-77.020945, 38.878241]
+                            'coordinates': [array_bussines[7].longitude, array_bussines[7].latitude]
                         }
                     }, {
                         'type': 'Feature',
                         'properties': {
-                            'description': '<strong>Truckeroo</strong><p>Truckeroo brings dozens of food trucks, live music, and games to half and M Street SE (across from Navy Yard Metro Station) today from 11:00 a.m. to 11:00 p.m.</p>'
+                            'description': `<strong>${array_bussines[8].name}</strong><p>Truckeroo brings dozens of food trucks, live music, and games to half and M Street SE (across from Navy Yard Metro Station) today from 11:00 a.m. to 11:00 p.m.</p>`
                         },
                         'geometry': {
                             'type': 'Point',
-                            'coordinates': [-77.007481, 38.876516]
+                            'coordinates': [array_bussines[8].longitude, array_bussines[8].latitude]
                         }
                     }]
                 }
